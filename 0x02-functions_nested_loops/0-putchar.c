fec980d446fc269d60588949570218ca561fc2b5
Keklem <keklemyevu@gmail.com>
2023-10-10 15:55:19 -0700

    has

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,19 @@
 #include "main.h"
-
-/*
- * main - print _putchar
-
- * is written by mbah
-
- * Description: This is used to print characters
-
+/**
+ * main - print putchar followed by a new line
+ * Description:you are not allowed to include standard libraries.
  * Return: 0
-
  */
-
-
 int main(void)
-
 {
-
 _putchar('_');
-
 _putchar('p');
-
 _putchar('u');
-
 _putchar('t');
-
 _putchar('c');
-
 _putchar('h');
-
 _putchar('a');
-
 _putchar('r');
-
 _putchar('\n');
-
 return (0);
-
 }
